773b29f810851dd641be583394073c6eb5786b4f
tatarinov <tatarinov@shogo.ru>
2018-07-30 10:44:24 +0300

    Скрипт тестирование mysql сервера

---
+++ mysql_test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -x
+INTERVAL=10
+PASSWORD='-uroot -p123'
+PREFIX=$INTERVAL-sec-status
+RUNFILE=/home/tatarinov/mysql/running
+mysql -e 'SHOW GLOBAL VARIABLES' $PASSWORD >> mysql-variables
+while test -e $RUNFILE; do
+   file=$(date +%F_%I)
+sleep=$(date +%s.%N | awk "{print $INTERVAL - (\$1 % $INTERVAL)}")
+   sleep $sleep
+   ts="$(date +"TS %s.%N %F %Т")"
+   loadavg="$(uptime)"
+   echo "$ts $loadavg" >> $PREFIX-${file}-status
+   mysql -e 'SHOW GLOBAL STATUS' $PASSWORD >> $PREFIX-$(file)-status &
+   echo "$ts $loadavg" >> $PREFIX-${file}-innodbstatus
+   mysql -e 'SHOW ENGINE INNODB STATUS\G' $PASSWORD  >> $PREFIX-${file}-innodbstatus  &
+   echo "$ts $loadavg" >> $PREFIX-${file}-processlist
+   mysql -e 'SHOW FULL PROCESSLIST\G' $PASSWORD >> $PREFIX-${file}-processlist & echo $ts
+done
+echo Exiting because $RUNFILE does not exist.
